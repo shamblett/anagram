fdf9ec1c9d41d98ab6274ed9274d26ea958a3890
Steve Hamblett <steve.hamblett@linux.com>
2020-04-01 14:20:55 +0100

    Initial port

--- bin/anagram.dart
+++ bin/anagram.dart
@@ -10,7 +10,15 @@ import 'dart:io';
 import 'package:args/args.dart';
 
 /// Anagram
-int main(List<String> args) {
+///
+///	Find all anagrams of a word or phrase which an be made from the words
+///	of the dictionary which can be supplied if you don't want to use the default one.
+/// All words containing characters not in the key are rejected out of
+/// hand so punctuated and capitalised words are usually lost.
+///	Eliminates all one-letter "words" except a, i, o.
+///
+///	originally from Martin Guy, December 1985
 
+int main(List<String> args) {
   return 0;
 }
--- lib/src/anagram.dart
+++ lib/src/anagram.dart
@@ -8,4 +8,30 @@
 part of anagram;
 
 /// The main anagram class
-class Anagram {}
+class Anagram {
+  /// Construction
+  Anagram();
+
+  /// Verbose.
+  bool verbose = false;
+
+  /// Maximum number of words to return in anagram.
+  int maxWords = 1;
+
+  /// Ignore case of initial letters of words from dictionary.
+  bool ignoreCaseInitial = false;
+
+  /// Ignore case of all letters of words from dictionary.
+  bool ignoreCaseAll = false;
+
+  /// Ignore punctuation in words from dictionary.
+  bool ignorePunctuation = false;
+
+  /// Dictionary path
+  String dictionaryPath = 'default';
+
+  /// Solve the anagram
+  List<String> solve(String word) {
+    return <String>[];
+  }
+}
